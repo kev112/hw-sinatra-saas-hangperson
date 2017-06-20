f1ab089a57d7360941d92dbed2af2bdfbc08c3a3
tao liu <werewaswas@live.com>
2017-06-20 22:50:47 +0000

    a

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,13 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :wrong_guesses, :guesses, :word, :word_with_guesses
+  
   def initialize(word)
     @word = word
+    @wrong_guesses = ''
+    @guesses = ''
+    @word_with_guesses = '-' * word.length 
   end
 
   def self.get_random_word
@@ -18,5 +23,31 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
+  
+  def guess (letter)
+    if letter =~ /(\A\z|\W)/ || letter.nil?
+      raise(ArgumentError)
+    end
+    
+    if guesses.include?(letter.downcase) || wrong_guesses.include?(letter.downcase)
+      return false
+    end
+    
+    if @word.include?(letter)
+      @guesses << letter
+    else
+      @wrong_guesses << letter
+    end
+    
+  end
+  
+  def word_with_guesses
+    # turn word to array
+    # scan array for letter, returning an array of
+    # @word.split(//)
+    # @word_with_guesses[@word.index(letter)] = letter #assign letter to correct position of word_with_guesses
+  end
+  
+  
 
 end
